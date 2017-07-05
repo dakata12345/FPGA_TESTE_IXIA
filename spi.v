d37549eed69f2258b4170b0c6f3a66fd32f8e665
Ionescu Valentin-Alexandru <ionescuvalentin38@yahoo.com>
2017-07-05 13:25:34 +0300

    Modificari aduse modulului master

--- spi.v
+++ spi.v
@@ -10,35 +10,34 @@ module spi(
 				output [7:0] rx_data,
 				output  sclk,
 				output reg busy,
-				output reg ss,
-				output reg mosi);
+				output  ss,
+				output  mosi);
 				
 				localparam STATE_RST = 2'b00;
 				localparam STATE_IDLE = 2'b01;
 				localparam STATE_RUNNING = 2'b10;
 				
+				reg [7:0] rx_data_reg;
 				reg [1:0] next_state,state;
 				reg [7:0] cnt;
 				reg [2:0] cnt_2;
-				reg ss_reg;
+				reg ss_reg,mosi_reg;
+				reg clk_out;
+				
 				always@(posedge clk)
 							if (~ss_reg) cnt_2 <=0;
 									else if (cnt_2 == 7) cnt_2 = 3'bz;
 										else cnt_2 <= cnt_2 + 1;
-				always@(posedge clk)
-						if (~ss_reg) mosi <= tx_data[cnt_2];		
 				
-				reg clk_out;
+				always@(posedge clk)
+						if ((~ss_reg) & (cnt_2 !=3'bz)) mosi_reg <= tx_data[cnt_2];		
+				    			else mosi_reg <= 1'bz;
+								
 				
 				always@(posedge clk)
 							if (~rst) begin
-										 if(cpol == 0) begin
 										 cnt<=0;
-										 clk_out <=0;
-										 end
-										 else if (cpol == 1) begin
-										 cnt<=0;
-										 clk_out <=1;
+										 clk_out <=cpol;
 										 end
 								 else if (cnt == clk_div) begin
 													cnt <=0;
@@ -49,32 +48,48 @@ module spi(
 				always@(posedge clk)
 							if (~rst) state <= STATE_RST;
 								else state <= next_state;
-				
-				
-				assign sclk = (~ss_reg) ? 1'bz : clk_out;
+									
 				
 				always@(*)
 						case(state)
 						STATE_RST : begin
 										busy = 1;
 										ss_reg = 1;
-										mosi = 1'bx;
-										rx_data <= 0;
+										rx_data_reg = 0;
 										next_state = STATE_IDLE;
 										end
 						STATE_IDLE: begin
+										 rx_data_reg = 0;
 										 if (en) begin
 													busy = 1;
 													next_state = STATE_RUNNING;
 													end
 													else next_state = STATE_IDLE;
 										ss_reg = 1;
-										mosi = 1'bz;
 										busy = 0;
 										 end
 						STATE_RUNNING:	begin
 											busy = 1;
 											ss_reg = 0;
-											end
+											rx_data_reg = 0;
+											if (cnt_2 == 3'bz)
+										      	begin
+													busy = 0;
+													ss_reg = 1;
+													next_state = STATE_IDLE;
+													end
+											else next_state = STATE_RUNNING;
+											end	
+						default : begin
+										busy = 1;
+										ss_reg = 1;
+										rx_data_reg = 0;
+										next_state = STATE_IDLE;
+										end					
 						endcase
+						
+						assign mosi = mosi_reg;
+						assign sclk = (~ss_reg) ? 1'bz : clk_out;
+						assign rx_data = rx_data_reg;
+						assign ss = ss_reg;
 	endmodule					
