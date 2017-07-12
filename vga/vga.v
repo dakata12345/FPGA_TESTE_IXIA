8cf134341f1e237430098f7d965ffe0d49693c98
Ionescu Valentin-Alexandru <you@example.com>
2017-07-12 17:33:53 +0300

    modificari

--- vga/vga.v
+++ vga/vga.v
@@ -1,9 +1,9 @@
 module VGA_SYNC(
 				input clock,
 				input rst,
-				output reg h_sync,
-				output reg v_sync,
-				output reg active_zone,
+				output  h_sync,
+				output  v_sync,
+				output  active_zone,
 				output [10:0] x_pos, y_pos);
 				
 			
@@ -41,23 +41,12 @@ module VGA_SYNC(
 				end						
 				
 				
-				always@(posedge clock)
-							if ((h_counter < (h_visible_area + h_front_porch)) | (h_counter > (h_visible_area + h_front_porch + h_sync_pulse)))
-										h_sync <= 1;
-								else h_sync <= 0;		
+				assign h_sync = ((h_counter < (h_visible_area + h_front_porch)) | (h_counter > (h_visible_area + h_front_porch + h_sync_pulse))) ? 1 : 0;
+				assign v_sync = ((v_counter < (v_visible_area + v_front_porch)) | (v_counter > (v_visible_area + v_front_porch + v_sync_pulse))) ? 1 : 0;
+				assign active_zone = ((h_counter < h_visible_area) & (v_counter < v_visible_area)) ? 1 : 0;		
+					   
 				
-				always@(posedge clock)
-							if ((v_counter < (v_visible_area + v_front_porch)) | (v_counter > (v_visible_area + v_front_porch + v_sync_pulse)))
-										v_sync <= 1;
-								else v_sync <= 0;	
-			   
-				always@(posedge clock)
-							if ((h_counter < h_visible_area) & (v_counter < v_visible_area))
-										active_zone <= 1;
-								else active_zone <= 0;	
-								
 				assign x_pos = (active_zone) ? h_counter : 11'bz;
 				assign y_pos = (active_zone) ? v_counter : 11'bz;				
 
 endmodule
-			
